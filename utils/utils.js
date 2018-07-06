ee0a857544f8e859a9ee1bafad6acfa925a16325
denamyte <denamyte@gmail.com>
2018-07-06 17:23:01 +0300

    010_numbers part 1: Fibonacci Binet formula

--- utils/utils.js
+++ utils/utils.js
@@ -1,4 +1,12 @@
 
+function inputUntilNumber(query, initial) {
+    do {
+        var input = prompt(query, initial === undefined ? 0 : initial);
+        if (isNumeric(input)) return +input;
+        alert("Your input '" + input + "' is not numeric");
+    } while (true);
+}
+
 function isNumeric(n) {
     return !isNaN(parseFloat(n)) && isFinite(n);
 }
