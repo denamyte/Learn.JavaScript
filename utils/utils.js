c329d258add9e299802c10dcb155e9f9da088907
denamyte <denamyte@gmail.com>
2018-07-09 12:25:03 +0300

    010_numbers part 1: random numbers

--- utils/utils.js
+++ utils/utils.js
@@ -1,7 +1,26 @@
 
+function inputUntilNumberWithConditions(query, gtNumber, gteNumber, isInteger, initial) {
+    do {
+        var number = inputUntilNumber(query, initial === undefined ? 1 : initial);
+        if (gtNumber && number <= gtNumber) {
+            alert("Your number is not greater than " + gtNumber);
+            continue;
+        }
+        if (gteNumber && number < gtNumber) {
+            alert("Your number is not greater than or equal to " + gteNumber);
+            continue;
+        }
+        if (isInteger && number !== Math.round(number)) {
+            alert("Your number is not integer");
+            continue;
+        }
+        return number;
+    } while (true);
+}
+
 function inputUntilNumber(query, initial) {
     do {
-        var input = prompt(query, initial === undefined ? 0 : initial);
+        var input = prompt(query === undefined ? "Input a number" : query, initial === undefined ? 0 : initial);
         if (isNumeric(input)) return +input;
         alert("Your input '" + input + "' is not numeric");
     } while (true);
