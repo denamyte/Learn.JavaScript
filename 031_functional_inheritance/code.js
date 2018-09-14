e2cabf74350533045430e4456681c4ab924dc958
denamyte <denamyte@gmail.com>
2018-09-14 14:59:24 +0300

    031_functional_inheritance: part 4

--- 031_functional_inheritance/code.js
+++ 031_functional_inheritance/code.js
@@ -152,7 +152,7 @@ task4.Machine = function (power) {
 
 
 task4.Fridge = function (power) {
-    task3.Machine.apply(this, arguments);
+    task4.Machine.apply(this, arguments);
 
     var food = [];
     var capacity = Math.floor(power / 100);
@@ -187,8 +187,79 @@ task4.Fridge = function (power) {
         return food.filter(func);
     };
 
-    this.removeFood = function (foodName) {
 
+    this.removeFood = function (removeItem) {
+        var removeFoodFilter = function (foodItem) {
+            return foodItem.title !== removeItem.title;
+        };
+        food = this.filterFood(removeFoodFilter)
+    }
+
+};
+
+var task5 = {};
+
+task5.Machine = function (power) {
+    this._power = power;
+    this._enabled = false;
+
+    var self = this;
+
+    this.enable = function () {
+        self._enabled = true;
+    };
+
+    this.disable = function () {
+        self._enabled = false;
+    };
+};
+
+
+task5.Fridge = function (power) {
+    task5.Machine.apply(this, arguments);
+
+    var food = [];
+    var capacity = Math.floor(power / 100);
+
+    var self = this;
+
+    this.addFood = function () {
+        // Checking the fridge is enabled
+        if (!this._enabled) {
+            throw new Error("The fridge is not enabled!");
+        }
+        var newCapacity = food.length + arguments.length;
+
+        if (newCapacity > capacity) {
+            throw new Error("The fridge capacity exceeded! " + newCapacity + "/" + capacity);
+        }
+        for (var i = 0; i < arguments.length; i++) {
+            food.push(arguments[i]);
+        }
+    };
+
+    this.getFood = function () {
+        return food.slice(0);
+    };
+
+    this.filterFood = function(func) {
+        return food.filter(func);
+    };
+
+
+    this.removeFood = function (removeItem) {
+        var removeFoodFilter = function (foodItem) {
+            return foodItem.title !== removeItem.title;
+        };
+        food = this.filterFood(removeFoodFilter)
+    };
+
+    var parentDisable = this.disable;
+    this.disable = function () {
+        if (food.length > 0) {
+            throw new Error("You cannot disable the fridge when there is food in it");
+        }
+        parentDisable();
     }
 
 };
