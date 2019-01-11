9e62e29e860bd6cd10007eadceba41c62f0de196
denamyte <denamyte@gmail.com>
2019-01-11 16:37:48 +0200

    036_modif_the_doc: part 2, calendar, halfway

--- s_10_document/036_modif_the_doc/code.js
+++ s_10_document/036_modif_the_doc/code.js
@@ -47,7 +47,7 @@ function createTree(parent, data) {
     }
 }
 
-/** Adds numbers of descendants in non-empty leaves of the list
+/** Adds numbers of descendants in non-empty list items of the list
  * 
  * @param {HTMLUListElement | HTMLOListElement} list 
  */
@@ -73,6 +73,10 @@ function showDescendantsNumbers(list) {
     return count;
 }
 
+/** Adds numbers of descendants in non-empty list items of the list
+ * 
+ * @param {*} list 
+ */
 function showDescendantsNumbers2(list) {
     let lis = list.getElementsByTagName('li');
 
@@ -85,3 +89,56 @@ function showDescendantsNumbers2(list) {
       li.firstChild.data += ' [' + descendantsCount + ']';
     }
 }
+
+let DAYS_OF_WEEK = ['MO', 'TU', 'WE', 'TH', 'FR', 'SA', 'SU'];
+/** Creates a calendar as a table 
+ * 
+ * @param {HTMLElement} elem An element where to put the table with the calendar
+ * @param {Number} year The year number
+ * @param {Number} month 1-based (!) number of the month
+ */
+function createCalendar(elem, year, month) {
+    // What day of the week is the first day of the given month
+    // let date = new Date(year, month - 1, 1);
+    let dayOfTheWeek = new Date(year, month - 1, 1).getDay();
+    console.log(`day of the week : ${dayOfTheWeek}`);
+
+    // How many days in the given month?
+    let daysInMonth = new Date(year, month, 0).getDate();
+    console.log(`days in the month : ${daysInMonth}`);
+
+    // How many days should the calendar table have (minimum)
+    let minimumDays = dayOfTheWeek - 1 + daysInMonth;
+    console.log(`minimum days : ${minimumDays}`);
+
+    // How many rows should the calendar table have,
+    // given that it must be a multiple of 7
+    let rowsNumber = Math.ceil(minimumDays / 7);
+    console.log(`number of rows : ${rowsNumber}`);
+
+    // The array which successively holds all days strings of the calendar table
+    let daysArray = new Array(rowsNumber * 7).fill('');
+    let curDay = 1;
+    for (let index = dayOfTheWeek - 1; index < dayOfTheWeek - 1 + daysInMonth; index++) {
+        daysArray[index] = curDay++;
+    }
+    console.log(`array of table days ${daysArray}`);
+    
+    // Generating the table: th
+    let table = document.createElement('table');
+    let trh  = document.createElement('tr');
+    for (let index = 0; index < 7; index++) {
+        let td = document.createElement('th');
+        td.innerText = DAYS_OF_WEEK[index];
+        trh.appendChild(td);
+    }
+    table.appendChild(trh);
+
+    // TODO ... Append other childs
+
+    // Assigning the table to elem
+    elem.appendChild(table);
+    
+}
+
+
